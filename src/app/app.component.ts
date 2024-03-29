c499ed0916caba0f79c88ad6914c74d378db15eb
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-03-29 22:59:51 +0300

    angular 17 eğitimi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'InspimoAcademyBootCamp';
+ works:string="";
+ title:string="ali can";
+ todos:string[]=[];
+ save(){
+  console.log(this.works);
+  this.todos.push(this.works);
+  this.works="";
+ }
 }
